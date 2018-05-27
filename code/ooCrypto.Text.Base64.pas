2b87824a74fbda1905c74c0c31d51a86ff7e4b47
Vencejo Software <vencejosoftware@gmail.com>
2018-05-26 21:20:48 -0300

    Fixed problem with conversion Wide/Ansi char

--- code/ooCrypto.Text.Base64.pas
+++ code/ooCrypto.Text.Base64.pas
@@ -40,9 +40,9 @@ type
   TCryptoTextBase64 = class sealed(TInterfacedObject, ICryptoText)
   strict private
   const
-    ENCODE_TABLE: array [1 .. 65] of Char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    ENCODE_TABLE: array [1 .. 65] of AnsiChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
   private
-    function FindInTable(const Letter: Char): Byte;
+    function FindInTable(const Letter: AnsiChar): Byte;
   public
     function Encode(const Text: string): string;
     function Decode(const Text: string): string;
@@ -51,7 +51,7 @@ type
 
 implementation
 
-function TCryptoTextBase64.FindInTable(const Letter: Char): Byte;
+function TCryptoTextBase64.FindInTable(const Letter: AnsiChar): Byte;
 begin
   Result := Pred(Pos(Letter, ENCODE_TABLE));
 end;
@@ -60,42 +60,47 @@ function TCryptoTextBase64.Decode(const Text: string): string;
 var
   SrcLen, Times, i: integer;
   x1, x2, x3, x4, xt: Byte;
+  Encoded, Decoded: AnsiString;
 begin
-  Result := EmptyStr;
-  SrcLen := length(Text);
+  Decoded := EmptyAnsiStr;
+  Encoded := AnsiString(Text);
+  SrcLen := Length(Text);
   Times := SrcLen div 4;
   for i := 0 to Pred(Times) do
   begin
-    x1 := FindInTable(Text[1 + i * 4]);
-    x2 := FindInTable(Text[2 + i * 4]);
-    x3 := FindInTable(Text[3 + i * 4]);
-    x4 := FindInTable(Text[4 + i * 4]);
+    x1 := FindInTable(Encoded[1 + i * 4]);
+    x2 := FindInTable(Encoded[2 + i * 4]);
+    x3 := FindInTable(Encoded[3 + i * 4]);
+    x4 := FindInTable(Encoded[4 + i * 4]);
     x1 := Byte(x1 shl 2);
     xt := Byte(x2 shr 4);
     x1 := x1 or xt;
     x2 := Byte(x2 shl 4);
-    Result := Result + Char(x1);
+    Decoded := Decoded + AnsiChar(x1);
     if x3 = 64 then
       break;
     xt := Byte(x3 shr 2);
     x2 := x2 or xt;
     x3 := Byte(x3 shl 6);
-    Result := Result + Char(x2);
+    Decoded := Decoded + AnsiChar(x2);
     if x4 = 64 then
       break;
     x3 := x3 or x4;
-    Result := Result + Char(x3);
+    Decoded := Decoded + AnsiChar(x3);
   end;
+  Result := String(Decoded);
 end;
 
 function TCryptoTextBase64.Encode(const Text: string): string;
 var
   Times, LenSrc, i: integer;
-  x1, x2, x3, x4: Char;
+  x1, x2, x3, x4: AnsiChar;
   xt: Byte;
+  Encoded, Decoded: AnsiString;
 begin
-  Result := EmptyStr;
-  LenSrc := length(Text);
+  Encoded := EmptyAnsiStr;
+  Decoded := AnsiString(Text);
+  LenSrc := Length(Decoded);
   if LenSrc mod 3 = 0 then
     Times := LenSrc div 3
   else
@@ -104,37 +109,38 @@ begin
   begin
     if LenSrc >= (3 + i * 3) then
     begin
-      x1 := ENCODE_TABLE[(ord(Text[1 + i * 3]) shr 2) + 1];
-      xt := Byte(ord(Text[1 + i * 3]) shl 4) and 48;
-      xt := xt or (ord(Text[2 + i * 3]) shr 4);
+      x1 := ENCODE_TABLE[(ord(Decoded[1 + i * 3]) shr 2) + 1];
+      xt := Byte(ord(Decoded[1 + i * 3]) shl 4) and 48;
+      xt := xt or (ord(Decoded[2 + i * 3]) shr 4);
       x2 := ENCODE_TABLE[xt + 1];
-      xt := Byte(ord(Text[2 + i * 3]) shl 2) and 60;
-      xt := xt or (ord(Text[3 + i * 3]) shr 6);
+      xt := Byte(ord(Decoded[2 + i * 3]) shl 2) and 60;
+      xt := xt or (ord(Decoded[3 + i * 3]) shr 6);
       x3 := ENCODE_TABLE[xt + 1];
-      xt := (ord(Text[3 + i * 3]) and 63);
+      xt := (ord(Decoded[3 + i * 3]) and 63);
       x4 := ENCODE_TABLE[xt + 1];
     end
     else
       if LenSrc >= (2 + i * 3) then
       begin
-        x1 := ENCODE_TABLE[(ord(Text[1 + i * 3]) shr 2) + 1];
-        xt := Byte(ord(Text[1 + i * 3]) shl 4) and 48;
-        xt := xt or (ord(Text[2 + i * 3]) shr 4);
+        x1 := ENCODE_TABLE[(ord(Decoded[1 + i * 3]) shr 2) + 1];
+        xt := Byte(ord(Decoded[1 + i * 3]) shl 4) and 48;
+        xt := xt or (ord(Decoded[2 + i * 3]) shr 4);
         x2 := ENCODE_TABLE[xt + 1];
-        xt := Byte(ord(Text[2 + i * 3]) shl 2) and 60;
+        xt := Byte(ord(Decoded[2 + i * 3]) shl 2) and 60;
         x3 := ENCODE_TABLE[xt + 1];
         x4 := '=';
       end
       else
       begin
-        x1 := ENCODE_TABLE[(ord(Text[1 + i * 3]) shr 2) + 1];
-        xt := Byte(ord(Text[1 + i * 3]) shl 4) and 48;
+        x1 := ENCODE_TABLE[(ord(Decoded[1 + i * 3]) shr 2) + 1];
+        xt := Byte(ord(Decoded[1 + i * 3]) shl 4) and 48;
         x2 := ENCODE_TABLE[xt + 1];
         x3 := '=';
         x4 := '=';
       end;
-    Result := Result + x1 + x2 + x3 + x4;
+    Encoded := Encoded + x1 + x2 + x3 + x4;
   end;
+  Result := String(Encoded);
 end;
 
 class function TCryptoTextBase64.New: ICryptoText;
